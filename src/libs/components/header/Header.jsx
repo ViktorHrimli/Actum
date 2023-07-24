3bdbe5ad9ef0bfd2659bcf98a465033e7a71981b
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-07-24 17:18:07 +0300

    Craete header and style, add file hooks

--- src/libs/components/header/Header.jsx
+++ src/libs/components/header/Header.jsx
@@ -1,14 +1,27 @@
-import Link from "next/link";
+"use client";
+import Image from "next/image";
 
 import Navigation from "@/libs/components/nav/Navigation";
+
 import { PathsPageHeader } from "./enums/enum";
+import { usePathname } from "@/libs/hooks/hooks";
+
+import Logo from "@/assets/svg/LOGO.png";
 
 import styles from "./Header.module.scss";
 
 export default function Header() {
+  const path = usePathname();
+
   return (
-    <div>
-      <Navigation links={PathsPageHeader} />
-    </div>
+    <section className={styles.header_section}>
+      <div className={styles.header_conteiner}>
+        <Image src={Logo} alt="Logo" width={40} height={37} />
+
+        <div className={styles.nav_conteiner}>
+          <Navigation links={PathsPageHeader} route={path} />
+        </div>
+      </div>
+    </section>
   );
 }
