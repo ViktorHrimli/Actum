d6195b41797cdb1e75f4f3c68fbe44d0b0dbd467
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-07-25 20:37:36 +0300

    Create hero section on desktop

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.scss";
+
+import Hero from "@/libs/components/hero/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
--- src/libs/enums/enum.js
+++ src/libs/enums/enum.js
@@ -1,1 +1,2 @@
 export { META_DATA_DESCRIPTION, META_DATA_TITLE } from "./app/app";
+export { ButtonsText } from "./button/buttons";
