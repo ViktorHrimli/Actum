6b8274935546fe542c93b044320fcd569428d698
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-07-26 22:06:11 +0300

    Add picture style and button

--- src/libs/components/about_company/AboutCompany.jsx
+++ src/libs/components/about_company/AboutCompany.jsx
@@ -1,16 +1,71 @@
 import Image from "next/image";
 
 import styles from "./AboutCompany.module.scss";
+import gradient from "./AboutGradients.module.scss";
 
 import XO from "@/assets/svg/XO.png";
 import XN from "@/assets/svg/XN.png";
+import Ukraine from "@/assets/svg/Ukraine.png";
+
+import Olga from "@/assets/svg/Olga.png";
+import Natalia from "@/assets/svg/Natalia.png";
+
 import Button from "../button/Button";
 
 export default function AboutCompany() {
   const handleClick = () => {};
   return (
     <section className={styles.section}>
-      <div>AAAAAAAAAAAAAA</div>
+      <div className={styles.conteiner_map}>
+        <div className={styles.ukraine_inner_text}>
+          <p>
+            Працюємо по <span className={styles.bold_text}>всій Україні</span>:
+            надаємо онлайн-консультації або виїзджаємо до Вас.
+          </p>
+        </div>
+        <div className={styles.ukraine_inner_city}>
+          <p>
+            Прямо сьогодні Ви можете завітати в офіс у 9 містах України:
+            <span className={styles.bold_text}>
+              Київ, Львів, Дніпро, Миколаїв, Житомир, Кривий Ріг, Херсон,
+              Покровськ.
+            </span>
+            : надаємо онлайн-консультації або виїзджаємо до Вас.
+          </p>
+        </div>
+        <Image
+          src={Ukraine}
+          alt="Ukraine"
+          width={650}
+          height={448}
+          style={{ position: "absolute", top: "-40px", left: "0px" }}
+        />
+
+        {/* GRADIENTS */}
+        <div className={gradient.main_gradient}></div>
+        <div className={gradient.second_gradient}></div>
+        {/* PHOTO */}
+        <div className={styles.olga_conteiner}>
+          <Image
+            src={Olga}
+            alt="Olga"
+            width={430}
+            height={700}
+            loading="lazy"
+            placeholder="blur"
+          />
+        </div>
+        <div className={styles.natalia_conteiner}>
+          <Image
+            src={Natalia}
+            alt="Natalia"
+            width={526}
+            height={745}
+            loading="lazy"
+            placeholder="blur"
+          />
+        </div>
+      </div>
       <div className={styles.about_conteiner}>
         <h2 className={styles.title_text}>Про компанію</h2>
         <p className={styles.text}>
@@ -48,6 +103,7 @@ export default function AboutCompany() {
               height={88}
               loading="lazy"
               alt="Підпис"
+              className={styles.signature}
             />
           </div>
         </div>
