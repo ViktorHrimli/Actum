d939e84525dce8deb6afab8b4c403fd629d2ec15
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-07-28 00:15:04 +0300

    fix bag

--- src/libs/components/ourDirections/criminalMatters/CriminalMattrs.jsx
+++ src/libs/components/ourDirections/criminalMatters/CriminalMattrs.jsx
@@ -1,23 +1,37 @@
-"use client";
 import Image from "next/image";
 
-import LinkedPath from "@/assets/svg/Linked-Path.png";
-
+import LinkedPath from "@/assets/svg/Linked_Path.png";
 
 import styles from "./CriminalMattrs.module.scss";
 
+export default function CriminalMattrs() {
+  return (
+    <div className={styles.criminal}>
+      <h2 className={styles.criminal_title}>кримінальні питання</h2>
 
+      <p className={styles.criminal_text}>
+        Кримінальні адвокати Актуму, спеціалісти з досвідом роботи у
+        прокуратурі, знаходять вади у доказах слідства та формують доказову базу
+        на користь клієнта.
+      </p>
+      <p className={styles.criminal_text}>
+        Досконале знання судової практики по окремим категоріям справ дозволяє
+        правникам Актуму знайти підстави для перекваліфікації складу злочину на
+        менш тяжку статтю КК, а нерідко – взагалі для закриття справи.
+      </p>
+      <p className={styles.criminal_text}>
+        Наші спеціалісти здатні проконтролювати дотримання стандартів утримання
+        в СІЗО та містах позбавлення волі. Маємо сильну експертизу у справах по
+        наркотиках, ДТП з тяжкими наслідками, службовій діяльності.
+      </p>
 
-export default function CriminalMattrs() { 
-  return <div className={styles.criminal}>
-
-    <h2 className={styles.criminal_title}>кримінальні питання</h2>
-    
-    <p className={styles.criminal_text}>Кримінальні адвокати Актуму, спеціалісти з досвідом роботи у прокуратурі, знаходять вади у доказах слідства та формують доказову базу на користь клієнта.</p>
-    <p className={styles.criminal_text}>Досконале знання судової практики по окремим категоріям справ дозволяє правникам Актуму знайти підстави для перекваліфікації складу злочину на менш тяжку статтю КК, а нерідко – взагалі для закриття справи.</p>
-    <p className={styles.criminal_text}>Наші спеціалісти здатні проконтролювати дотримання стандартів утримання в СІЗО та містах позбавлення волі. Маємо сильну експертизу у справах по наркотиках, ДТП з тяжкими наслідками, службовій діяльності.</p>
-    
-    <Image src={LinkedPath} alt="Linked" width={180} height={180} className={styles.criminal_icon}/>
-
+      <Image
+        src={LinkedPath}
+        alt="Linked"
+        width={180}
+        height={180}
+        className={styles.criminal_icon}
+      />
     </div>
+  );
 }
