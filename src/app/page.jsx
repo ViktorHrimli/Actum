b560c4b1beac0bea4afeb590981b46343c83c032
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-07-28 18:55:25 +0300

    enought fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,13 +6,11 @@ import Direction from "@/libs/components/ourDirections/Direction";
 import AboutCompany from "@/libs/components/about_company/AboutCompany";
 import Response from "@/libs/components/response/Response";
 
-
 export default function Home() {
   return (
     <>
       <ContactPanel />
       <Hero />
-
       <Direction />
       <AboutCompany />
       <Response />
