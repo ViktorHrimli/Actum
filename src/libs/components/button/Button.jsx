0e9e9508efb016dc72b709d5ef871b1aa1ca6463
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-08-05 13:00:29 +0300

    make gradient

--- src/libs/components/button/Button.jsx
+++ src/libs/components/button/Button.jsx
@@ -2,7 +2,7 @@ import styles from "./Button.module.scss";
 
 import { ButtonsText } from "@/libs/enums/enum";
 
-export default function Button({ onClick, type, text, style }) {
+export default function Button({ onClick, type, text, style, width }) {
   return (
     <button type={type} className={styles[style]}>
       {text}
