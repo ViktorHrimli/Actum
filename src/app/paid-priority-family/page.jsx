a90ca2ad256efd2217b2f8b686254ea2be37f611
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-08-12 19:21:49 +0300

    add section question and animate accordion block

--- src/app/paid-priority-family/page.jsx
+++ src/app/paid-priority-family/page.jsx
@@ -5,7 +5,7 @@ import Specialists from "@/libs/pages/components/specialists/Specialists";
 import StepsLawyers from "@/libs/pages/components/stepLawyers/StepsLawyers";
 import Description from "@/libs/pages/services/components/description/Description";
 import Response from "@/libs/components/response/Response";
-
+import QuestionsList from "@/libs/pages/components/QuestionList/QuestionsList";
 
 export const metadata = {
   title: META_DATA_TITLE.BOOK,
@@ -16,7 +16,8 @@ export default function Family() {
   return (
     <>
       <HeroLawyers type={"family"} />
-      <Specialists type={"family"}/>
+      <QuestionsList type={"family"} />
+      <Specialists type={"family"} />
       <Description type={"family"} />
       <Response />
       <StepsLawyers />
