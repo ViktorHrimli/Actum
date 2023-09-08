610e90675532837240dabc2b19a7196379e19667
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-09-08 21:20:05 +0300

    fix bag

--- src/libs/components/hero/Watemark.jsx
+++ src/libs/components/hero/Watemark.jsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 
 import watemark from "@/assets/svg/Actum_HERO.png";
 
-import watemark from "@/assets/svg/Actum_HERO.png";
 export default function Watemark() {
   return (
     <Image src={watemark} alt="ACTUM" priority={true} placeholder="blur" fill />
