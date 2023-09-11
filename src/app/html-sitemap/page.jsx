86f9808b57591f44171fbd5f6c727d19baf2599a
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-09-11 19:11:19 +0300

    fix bag

---
+++ src/app/html-sitemap/page.jsx
@@ -0,0 +1,18 @@
+import { META_DATA_DESCRIPTION, META_DATA_TITLE } from "@/libs/enums/enum";
+
+import NestedHero from "@/libs/pages/components/nestedPageHero/NestedHero";
+
+import hero_img from "@/assets/svg/sitemap_hero.png";
+
+export const metadata = {
+  title: META_DATA_TITLE.BOOK,
+  description: META_DATA_DESCRIPTION.BOOK,
+};
+
+export default function page() {
+  return (
+    <>
+      <NestedHero img={hero_img} text={"мапа сайта"} />
+    </>
+  );
+}
