83c8557d02f48e3b6cb1a86cda1bbd8cc1a104f3
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-09-14 01:10:48 +0300

    fix-bag

--- src/libs/components/nav/SelectNav/Select.jsx
+++ src/libs/components/nav/SelectNav/Select.jsx
@@ -6,22 +6,28 @@ import NavSelectItem from "../NavSelectItem/NavSelectItem";
 
 import styles from "./Select.module.scss";
 
-export default function Select({ routes, isOpen, onClick }) {
+export default function Select({ routes, isOpen, onClick, isMobile }) {
   return (
     <AnimatePresence>
       <motion.div
+        key={"menu_conteiner"}
         initial={{ opacity: 0, y: -50 }}
-        animate={isOpen ? "open" : "step"}
-        variants={{ open: { y: 0, opacity: 1 }, step: { opacity: 0, y: -50 } }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.7 }}
+        exit={{ opacity: 0, y: -50 }}
         className={styles.conteiner}
       >
-        {routes && (
-          <ul className={styles.list}>
-            {routes.map((item, id) => (
-              <NavSelectItem key={id} {...item} onClick={onClick} />
-            ))}
-          </ul>
+        {routes && isOpen && (
+          <>
+            {!isMobile && (
+              <div className={styles.hidden_wrapper} onClick={onClick}></div>
+            )}
+            <ul className={styles.list}>
+              {routes.map((item, id) => (
+                <NavSelectItem key={id} {...item} onClick={onClick} />
+              ))}
+            </ul>
+          </>
         )}
       </motion.div>
     </AnimatePresence>
