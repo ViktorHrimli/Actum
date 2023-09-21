1d37b2ee6e54f71df34c0da2adb242ba939ec8cd
Savchenko-Andrey <dracxm95@gmail.com>
2023-09-21 16:36:20 +0300

    stules form country

---
+++ src/libs/components/arrow_menu/libs/enums.js
@@ -0,0 +1,16 @@
+const colorSvg = {
+  home: {
+    color: "#E32F7A",
+  },
+  family: {
+    color: "#E32F7A",
+  },
+  army: {
+    color: "#536641",
+  },
+  crime: {
+    color: "#3F2E64",
+  },
+};
+
+export { colorSvg };
