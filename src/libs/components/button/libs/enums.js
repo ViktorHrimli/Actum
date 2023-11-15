1e6defa4147e4538a008abd384744756c1c37113
Savchenko-Andrey <dracxm95@gmail.com>
2023-11-15 14:31:40 +0200

    generator sitemap

--- src/libs/components/button/libs/enums.js
+++ src/libs/components/button/libs/enums.js
@@ -15,6 +15,10 @@ const colorBorderEnums = {
     gradientBorder: "button_border",
     background: "background_criminal",
   },
+  other: {
+    gradientBorder: "button_border_other",
+    background: "background_family",
+  },
 };
 
 export { colorBorderEnums };
