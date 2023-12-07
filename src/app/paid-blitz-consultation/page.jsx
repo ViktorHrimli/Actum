149d1f77e4ad3c2a5cede6b308dbd99fc96641eb
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-12-07 13:04:36 +0200

    fix bag

--- src/app/paid-blitz-consultation/page.jsx
+++ src/app/paid-blitz-consultation/page.jsx
@@ -7,14 +7,16 @@ import QuestionsList from "@/libs/pages/components/QuestionList/QuestionsList";
 import SuccessfulBusiness from "@/libs/pages/components/successfulBusiness/SuccessfulBusiness";
 import FormSection from "@/shared/components/formSection/FormSection";
 import StructureData from "@/shared/components/structure_data_tamplate/StructureData";
+import AboutCards from "@/libs/pages/components/aboutCards/AboutCards";
+import Price from "@/libs/pages/components/priceCards/Price";
 
 import { getStaticLawyersPage } from "@/shared/services/api/api";
 import { makeSeoTemplate } from "@/shared/helpers/helpers";
 
-const { API_POLAND_LAWYER_PAGE } = process.env;
+const { API_BLITZ_CONSULTATION } = process.env;
 
 export async function generateMetadata() {
-  return makeSeoTemplate(API_POLAND_LAWYER_PAGE);
+  return makeSeoTemplate(API_BLITZ_CONSULTATION);
 }
 
 export default async function page() {
@@ -30,28 +32,33 @@ export default async function page() {
         questions_list,
         bread_crumbs,
         description_lawyer,
+        statistics,
         Steps: steps,
         successful_deals,
+        price_block,
+        title_price_block,
         seo,
       },
     },
-  } = await getStaticLawyersPage(API_POLAND_LAWYER_PAGE);
+  } = await getStaticLawyersPage(API_BLITZ_CONSULTATION);
 
   return (
     <>
       <StructureData data={seo["structuredData"]} />
-
       <HeroLawyers type={"other"} {...hero} bread_crumbs={bread_crumbs} />
+
       <QuestionsList
         type={"other"}
         about_block={about_block}
         questions={questions_list}
       />
+      <AboutCards type={"crime"} listCard={statistics} />
       <SuccessfulBusiness type={"other"} {...successful_deals} />
       <Specialists type={"other"} {...employeer_list} />
       <Description type={"other"} description={description_lawyer} />
       <Response type={"other"} {...responses} />
       <StepsLawyers type={"other"} {...steps} />
+      <Price type={"crime"} title={title_price_block} list={price_block} />
       <FormSection type={"other"} formData={form} {...info} />
     </>
   );
--- src/app/paid-priority-consultation/page.jsx
+++ src/app/paid-priority-consultation/page.jsx
@@ -7,52 +7,59 @@ import QuestionsList from "@/libs/pages/components/QuestionList/QuestionsList";
 import SuccessfulBusiness from "@/libs/pages/components/successfulBusiness/SuccessfulBusiness";
 import FormSection from "@/shared/components/formSection/FormSection";
 import StructureData from "@/shared/components/structure_data_tamplate/StructureData";
+import AboutCards from "@/libs/pages/components/aboutCards/AboutCards";
+import Price from "@/libs/pages/components/priceCards/Price";
 
 import { getStaticLawyersPage } from "@/shared/services/api/api";
 import { makeSeoTemplate } from "@/shared/helpers/helpers";
 
-const { API_REALTY_PAGE } = process.env;
+const { API_CONSULTATION_JURIST } = process.env;
 
-// export async function generateMetadata({ params, searchParams }, parent) {
-//   return makeSeoTemplate(API_REALTY_PAGE);
-// }
+export async function generateMetadata() {
+  return makeSeoTemplate(API_CONSULTATION_JURIST);
+}
 
 export default async function page() {
-  // const {
-  //   data: {
-  //     attributes: {
-  //       Hero: hero,
-  //       about_block,
-  //       Employeers_list: employeer_list,
-  //       Form: form,
-  //       Info: info,
-  //       Responses: responses,
-  //       questions_list,
-  //       bread_crumbs,
-  //       description_lawyer,
-  //       Steps: steps,
-  //       successful_deals,
-  //       seo,
-  //     },
-  //   },
-  // } = await getStaticLawyersPage(API_REALTY_PAGE);
+  const {
+    data: {
+      attributes: {
+        Hero: hero,
+        about_block,
+        Employeers_list: employeer_list,
+        Form: form,
+        Info: info,
+        Responses: responses,
+        questions_list,
+        bread_crumbs,
+        description_lawyer,
+        statistics,
+        Steps: steps,
+        successful_deals,
+        price_block,
+        title_price_block,
+        seo,
+      },
+    },
+  } = await getStaticLawyersPage(API_CONSULTATION_JURIST);
 
   return (
     <>
-      {/* <StructureData data={seo["structuredData"]} />
-
+      <StructureData data={seo["structuredData"]} />
       <HeroLawyers type={"other"} {...hero} bread_crumbs={bread_crumbs} />
+
       <QuestionsList
         type={"other"}
         about_block={about_block}
         questions={questions_list}
       />
+      <AboutCards type={"crime"} listCard={statistics} />
       <SuccessfulBusiness type={"other"} {...successful_deals} />
       <Specialists type={"other"} {...employeer_list} />
       <Description type={"other"} description={description_lawyer} />
       <Response type={"other"} {...responses} />
       <StepsLawyers type={"other"} {...steps} />
-      <FormSection type={"other"} formData={form} {...info} /> */}
+      <Price type={"crime"} title={title_price_block} list={price_block} />
+      <FormSection type={"other"} formData={form} {...info} />
     </>
   );
 }
