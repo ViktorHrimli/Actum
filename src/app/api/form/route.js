ff5ddb04fc9060bf8ad5b5b9b28e87329e39108a
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-12-23 22:29:31 +0200

    fix bag

--- src/app/api/form/route.js
+++ src/app/api/form/route.js
@@ -3,8 +3,6 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
   const data = await req.json();
 
-  console.log(data, "SEND FORM");
-
   const res = await fetch(
     "https://script.google.com/macros/s/AKfycbxKAn8CRbPEUWYsqLm9wg7EwMHGUJHfbhsrEnXAkWPDyJUlKaVXTyHrWk8OMob0tkmHQw/exec",
     {
